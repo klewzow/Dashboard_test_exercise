25b9c6d6e28e16b85506d7f93cfdd81af6d3a5ad
Dmitry <klewzow@gmail.com>
2022-10-20 23:04:17 +0300

    odyfy

--- src/main/java/com/gmail/klewzow/Configurations/ThymeleafConfiguration.java
+++ src/main/java/com/gmail/klewzow/Configurations/ThymeleafConfiguration.java
@@ -12,9 +12,6 @@ import org.thymeleaf.templatemode.TemplateMode;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-import java.io.File;
 
 
 @Configuration
--- src/main/java/com/gmail/klewzow/Controllers/IndexController.java
+++ src/main/java/com/gmail/klewzow/Controllers/IndexController.java
@@ -1,6 +1,5 @@
 package com.gmail.klewzow.Controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +11,6 @@ public class IndexController {
 
     @GetMapping("/")
     public String indexPage()  {
-
-
-
         return "index";
     }
 }
